acc9cfdb77f17282a4ab2a1567bcd71fdb3851a3
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2025-08-07 17:40:11 -0300

    New workflow

--- src/types.py
+++
@@ -1,5 +0,0 @@
-from typing import Dict
-
-class StateSchema(Dict):
-  request: str
-  email: str
--- src/workflow.py
+++ src/workflow.py
@@ -1,9 +1,12 @@
 from langgraph.graph import END, START
 from src.state import workflow
-from src.nodes import write_email_node
+from src.nodes import call_llm, run_tool, should_continue
 
-workflow.add_node("write_email_node", write_email_node)
-workflow.add_edge(START, "write_email_node")
-workflow.add_edge("write_email_node", END)
+workflow.add_node("call_llm", call_llm)
+workflow.add_node("run_tool", run_tool)
+
+workflow.add_edge(START, "call_llm")
+workflow.add_conditional_edges("call_llm", should_continue, {"run_tool": "run_tool", END: END})
+workflow.add_edge("run_tool", END)
 
 app = workflow.compile()
